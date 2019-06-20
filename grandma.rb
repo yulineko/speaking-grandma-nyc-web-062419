58664a43333105f9d81f303b260a9ff542a2d03d
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-06-20 21:37:34 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,14 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+
+def speak_to_grandma
+  if "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+  elsif "Hi Nana, how are you?" 
+    puts "HUH?! SPEAK UP, SONNY!"
+  elsif "Hi!"
+    puts "HUH?! SPEAK UP, SONNY!"
+  
+end
